212f11e516bbe5065e90f420975a2a55e492f4ff
Anders Steen <anders.steen@knowit.no>
2023-07-17 16:30:02 +0200

    GPT is having trouble with list. Adding a new sample

--- src/zshgpt/cli/messages.py
+++ src/zshgpt/cli/messages.py
@@ -25,6 +25,15 @@ messages = [
     },
     {'role': 'user', 'content': '# open it with vi'},
     {'role': 'assistant', 'content': 'vi web-app/index.html'},
+    {'role': 'user', 'content': '# How do I make a greek salad explained in 4 lines or less'},
+    {'role': 'assistant', 'content': """# To make a Greek salad, you will need:
+# 1. Chop tomatoes, cucumbers, and red onions.
+# 2. Add feta cheese, Kalamata olives, and fresh herbs like oregano.
+# 3. Drizzle olive oil and lemon juice as dressing.
+# 4. Toss everything together and serve.
+
+# Enjoy your Greek salad!
+"""},
     {'role': 'user', 'content': "# what's running on port 1018?"},
     {'role': 'assistant', 'content': 'lsof -i :1018'},
     {'role': 'user', 'content': '# kill process 1584'},
@@ -38,22 +47,6 @@ messages = [
         'role': 'assistant',
         'content': """# Sure, here's a joke for you: Why did the tomato turn red?
 
-Because it saw the salad dressing!""",
-    },
-    {'role': 'user', 'content': '# Another one, but multiple lines'},
-    {
-        'role': 'assistant',
-        'content': """# Sure, here's a multiline joke for you:
-
-# Why don't skeletons fight each other?
-# They don't have the guts.
-
-# Why did the scarecrow win an award?
-# Because he was outstanding in his field.
-
-# Why don't scientists trust atoms?
-# Because they make up everything.
-
-# I hope you enjoyed the jokes!""",
+# Because it saw the salad dressing!""",
     },
 ]
