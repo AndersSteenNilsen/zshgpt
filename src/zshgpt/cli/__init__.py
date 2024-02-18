545bf3b2c791384be61fdb545f2ed8fbc5a81306
Anders Steen <anders.steen@knowit.no>
2024-02-18 17:51:54 +0100

    Upping openai and using only ruff

--- src/zshgpt/cli/__init__.py
+++ src/zshgpt/cli/__init__.py
@@ -2,11 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 import click
-import openai
-from openai.error import AuthenticationError
+from openai import AuthenticationError, OpenAI
 
 from zshgpt.__about__ import __version__
 from zshgpt.cli.messages import messages
+
+client = OpenAI()
 
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=True)
@@ -14,9 +15,9 @@ from zshgpt.cli.messages import messages
 @click.argument('user_query')
 def zshgpt(user_query: str) -> str:
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model='gpt-3.5-turbo', messages=[*messages, {'role': 'user', 'content': user_query}]
         )
     except AuthenticationError as auth_error:
         raise click.ClickException(auth_error.user_message) from auth_error
-    click.echo(response['choices'][0]['message']['content'], nl=False)
+    click.echo(response.choices[0].message.content, nl=False)
--- tests/test_no_key.py
+++ tests/test_no_key.py
@@ -1,11 +1,9 @@
-import openai
 from click.testing import CliRunner
 
 from src.zshgpt.cli import zshgpt
 
 
 def test_no_key():
-    openai.api_key = None
     runner = CliRunner()
     result = runner.invoke(zshgpt, ['# Can I speak with you?'])
     assert result.exit_code == 1
