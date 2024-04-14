e9e35075d546793669f2dbd91cd8101c8f7d0039
Anders Steen <anders.steen@knowit.no>
2024-04-14 17:26:22 +0200

    Fixing wrong key test

--- tests/test_invalid_key.py
+++ tests/test_invalid_key.py
@@ -1,10 +1,13 @@
+from os import environ
+
 from click.testing import CliRunner
 
 
 def test_invalid_key():
+    environ['OPENAI_API_KEY'] = 'WRONG_KEY'
     from src.zshgpt.cli import zshgpt
 
     runner = CliRunner()
-    result = runner.invoke(zshgpt, ['# Can I speak with you?'], env={'OPENAI_API_KEY': 'WRONG_KEY'})
+    result = runner.invoke(zshgpt, ['# Can I speak with you?'])
     assert result.exit_code == 1
     assert result.exception
